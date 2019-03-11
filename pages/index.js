9dffdb78c2efd032adf3204ae20bb53beda78d65
Daniel Park <dpeace926@gmail.com>
2019-03-10 23:48:29 -0400

    initialize next app

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from "react";
+import Link from "next/link";
+
+class index extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 />
+      </div>
+    );
+  }
+}
+
+export default index;
